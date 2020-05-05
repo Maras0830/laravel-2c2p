d459142f679ca5b84f7296b9982957991da72467
Maras Chen <Maras0830@gmail.com>
2020-05-05 16:08:20 +0800

    Update to_c_to_p.php

--- config/to_c_to_p.php
+++ config/to_c_to_p.php
@@ -4,13 +4,7 @@ return [
 
     'Debug' => env('CASH_STORE_DEBUG_MODE'),
 
-    /*
-     * 智付寶商店代號
-     */
     'MerchantID' => env('TOCTOP_MERCHANTID'),
 
-    /*
-     * 智付寶商店代號
-     */
     'SecretKey' => env('TOCTOP_SECRET_KEY'),
 ];
